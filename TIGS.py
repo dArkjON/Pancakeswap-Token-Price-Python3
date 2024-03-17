4b9973dda874f11bd09e7af375be0cbff0846990
Jon D <info@darkjon.de>
2024-03-17 14:15:32 +0100

    Update TIGS.py

--- TIGS.py
+++ TIGS.py
@@ -1,7 +1,6 @@
-import discord, json
-from discord.ext.commands import Bot
+import json
 from web3 import Web3
-import asyncio
+
 
 
 
@@ -9,24 +8,18 @@ import asyncio
 ###################################################################
 #######                       SETTINGS                      #######
 ###################################################################
-DISCORD_TOKEN = '<YOUR DISCORD TOKEN>'
-
-
 BEP20_QUOTETOKEN_ADDRESS = '0xe9e7cea3dedca5984780bafc599bd69add087d56' #<= BUSD 
 BEP20_QUOTETOKEN_DECIMALS = 18 #BUSD => 18
 
-
-BEP20_TOKEN_SYMBOL = "TIGS"
-BEP20_TOKEN = '0x34FaA80FEC0233e045eD4737cc152a71e490e2E3' #<= TIGS
-BEP20_TOKEN_DECIMALS = 18
-
+BEP20_TOKEN_SYMBOL = "sBTX"
+BEP20_TOKEN = '0x000000089fb24237dA101020Ff8e2AfD14624687' #<= sBTX Contract
+BEP20_TOKEN_DECIMALS = 8
 
 TRADINGTIGERSROUTER = '0xdEdf20172b6dC39817026c125f52d4fad8E0f29b' #This contract do all the work for you, find the best Pool and convert too your QuoteToken. <= (PancakeSwap)
 
 FINAL_PRICE_ROUND_TO_DECIMALS = 3
 ###################################################################
 
-bot = Bot(command_prefix="$") # Not used, but prefix is need.
 ABI = [{
         "inputs": [
             {
@@ -76,18 +69,5 @@ def get_TokenPrice():
         
     TokenTokenPrice = round(TOKEN_Price / (10**BEP20_QUOTETOKEN_DECIMALS), FINAL_PRICE_ROUND_TO_DECIMALS)
     return TokenTokenPrice
-    
-
-async def status_update():
-    while True:
-        price = str(BEP20_TOKEN_SYMBOL) + " | " + str(get_TokenPrice()) + " $"
-        print(price)
-        await bot.user.edit(username=price)
-        await asyncio.sleep(1800)
-
-@bot.event
-async def on_ready():
-    print(f'Bot connected as {bot.user}')
-    bot.loop.create_task(status_update())
-		
-bot.run(DISCORD_TOKEN)
+	
+print (get_TokenPrice())
